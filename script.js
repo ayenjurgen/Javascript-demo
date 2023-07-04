8864e2f3fecc77e007cc4cb628d7a8fdc7d27b8d
ayenjurgen <ayenjurgen>
2023-07-04 20:49:23 +0800

    1st java lesson

---
+++ script.js
@@ -0,0 +1,55 @@
+// alert("Hello World");
+
+// console.log("hello world!");
+// // console.warn("warning message!");
+// // console.error("error message");
+// console.debug("debug message");
+// // console.dir();
+
+// ------Variables ------- //
+var firstName = "Juan";
+var lastName = "Dela Cruz";
+
+
+var firstName = "John";
+
+
+console.log(firstName);
+console.log(lastName);
+
+let fName = "John";
+let lName = "Doe";
+
+fName = "Jose";
+lName = "Rizal";
+
+console.log(fName);
+console.log(lName);
+
+const address = "Malacanang"; // String
+// fullName = "White House"; //error here
+
+
+//--------Data Types--------//
+
+const fullName = "Juan Dela Cruz"; // String
+const age = 20; //Integer or number
+const weight = 88.1; //Number float or decimal
+const isOVerweight = true; // (Boolean) true or false
+const undef = undefined; 
+const nullValue = null;
+
+console.log("\nData Type typeof");
+
+console.log(typeof fullName);
+console.log(typeof age);
+console.log(typeof weight);
+console.log(typeof isOVerweight);
+console.log(typeof undef);
+console.log(typeof nullValue);
+
+
+let v = "hello";
+console.log(v);
+v = 10;
+console.log(V);
